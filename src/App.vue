32713956c1c0122f23b7868cc2ae2871f2588719
= <=>
2022-06-29 16:29:42 +0700

    form register

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,128 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+  <b-container>
+    <b-card header="Form Submit">
+      <b-form @submit.prevent="onSubmit">
+
+        <b-form-group label="Name:">
+          <b-form-input v-model="form.name" placeholder="Enter name">
+          </b-form-input>
+        </b-form-group>
+
+        <b-form-group label="Email address:">
+          <b-form-input v-model="form.email" type="email" placeholder="Enter email">
+          </b-form-input>
+        </b-form-group>
+
+        <b-form-group label="Password:">
+          <b-form-input v-model="form.password" type="password">
+          </b-form-input>
+        </b-form-group>
+
+        <b-form-group label="Gender">
+          <b-form-radio v-model="form.gender" name="gender" value="male">Male</b-form-radio>
+          <b-form-radio v-model="form.gender" name="gender" value="female">Female</b-form-radio>
+          <b-form-radio v-model="form.gender" name="gender" value="other">Other</b-form-radio>
+        </b-form-group>
+
+        <b-form-group label="Languages:">
+          <b-form-checkbox-group v-model="form.languages" :options="languageList">           
+          </b-form-checkbox-group>
+        </b-form-group>
+
+        <b-form-group label="Birthday" id="selectDate">
+          <b-row>
+            <b-col md="4" class="d-flex">
+              <b-form-select v-model="form.year" @change="get_days">
+                <option v-for="n in 200" :value="n + 1899" :key="n">
+                {{ n + 1899 }} 
+                </option>
+              </b-form-select> 
+              <span class="ml-2">YEAR</span>
+            </b-col>
+            <b-col md="4" class="d-flex">
+              <b-form-select v-model="form.month" @change="get_days">
+                <option v-for="n in 12" :value="n" :key="n">
+                {{ n }}
+                </option>
+              </b-form-select>
+              <span class="ml-2">MONTH</span>
+            </b-col>
+            <b-col md="4" class="d-flex">
+              <b-form-select v-model="form.day">
+                <option v-for="n in form.days_max" :value="n" :key="n">
+                {{ n }} 
+                </option>
+              </b-form-select>
+              <span class="ml-2">DAYS</span>
+            </b-col>
+          </b-row>
+        </b-form-group>
+
+        <b-form-group label="Avatar:"   >
+          <b-form-file v-model="form.avatar" placeholder="Choose a file or drop it here..." >
+        </b-form-file>
+        </b-form-group>
+        
+
+        <b-form-textarea class="mb-3"  v-model="form.note" placeholder="Note something..." rows="3" max-rows="6">         
+        </b-form-textarea>
+
+        <b-button type="submit" variant="success">Submit</b-button>
+      </b-form>
+    </b-card>
+  </b-container>
 </template>
 
 <script>
+import languageList from './const/languages';
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+      form: {
+        name: "",
+        email: "",
+        password: "",
+        gender: null,
+        languages: [],
+        avatar: null,
+        note: "",
+        year: 1900,
+        month: 1,
+        day: 1,
+        days_max: '',
+      },
+      languageList: languageList
+    };
+  },
+  created: function () {
+                this.get_days();
+            },
+  methods: {
+    onSubmit(event) {
+      console.log(this.form.name)
+      console.log(this.form.email)
+      console.log(this.form.password)
+      console.log(this.form.gender)
+      console.log(this.form.languages)
+      let birthday = this.form.day + " / " + this.form.month + " / " + this.form.year;
+      console.log(birthday);
+      console.log(this.form.note)
+    },
+    get_days() {
+      this.form.days_max = new Date(this.form.year, this.form.month, 0).getDate(); 
+    } 
+  },
+};
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.card-header {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  background-color: #28a745;
 }
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.container {
+  margin: 20px auto;
+  width: 700px;
 }
 </style>
---
+++ src/const/languages.js
@@ -0,0 +1,6 @@
+export default [
+    { text: 'PHP', value: 'PHP' },
+    { text: 'JavaScript', value: 'JavaScript' },
+    { text: 'Laravel', value: 'Laravel' },
+    { text: 'Vue JS', value: 'Vue JS' }
+  ] 
